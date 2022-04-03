911b3bebe28c545133f07bbf8a64b8d8cde16121
Yuliia Shybyk <j7557500@gmail.com>
2022-04-03 22:16:48 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+      buttonStart.disabled = true;
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+});
